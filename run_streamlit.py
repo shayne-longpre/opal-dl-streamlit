75cdf676d4885b094d79e8468aee406d34411ead
Shayne Longpre <srlongpre13@gmail.com>
2023-07-09 13:39:17 -0400

    naana integration attempt 1

--- run_streamlit.py
+++ run_streamlit.py
@@ -38,17 +38,21 @@ def load_data():
 def insert_main_viz():
     components.html(
         """
+        <!DOCTYPE html>
+        <html lang="en">
         <head>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/p5.js"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/addons/p5.sound.min.js"></script>
             <link rel="stylesheet" type="text/css" href="style.css">
             <meta charset="utf-8" />
+
         </head>
         <body>
             <main>
             </main>
             <script src="dpi_viz/sketch.js"></script>
         </body>
+        </html>
         """,
         height=600,
     )
