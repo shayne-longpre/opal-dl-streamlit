1d90b2f6f7e9f2cec8bb05573af92fcecf0996c9
Shayne Longpre <srlongpre13@gmail.com>
2023-08-26 23:14:36 -0400

    update

--- run_streamlit.py
+++ run_streamlit.py
@@ -159,7 +159,7 @@ def streamlit_app():
 
                     format_multiselect = st.multiselect(
                         'Select the format types to cover in your datasets',
-                        ["All"] + list(INFO["constants"]["FORMATS"].keys()),
+                        ["All"] + INFO["constants"]["FORMATS"],
                         ["All"])
 
                     domain_multiselect = st.multiselect(
