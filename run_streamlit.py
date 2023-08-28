bae261c188065cf60cc6b46f2bccfda3710da156
Shayne Longpre <srlongpre13@gmail.com>
2023-08-28 16:04:00 -0400

    update

--- run_streamlit.py
+++ run_streamlit.py
@@ -55,11 +55,11 @@ def insert_main_viz():
     components.html(sketch, height=800, scrolling=True)
 
 def insert_plot_viz1():
-    sketch = '<script src="https://d3js.org/d3-hexbin.v0.2.min.js"></script>'
-    sketch += '<script src="https://d3js.org/d3.v5.min.js"></script>'
+    sketch = '<script type="module" src="https://d3js.org/d3-hexbin.v0.2.min.js"></script>'
+    sketch += '<script type="module" src="https://d3js.org/d3.v5.min.js"></script>'
 
-    sketch += '<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/p5.js"></script>'
-    sketch += '<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/addons/p5.sound.min.js"></script>'
+    sketch += '<script type="module" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/p5.js"></script>'
+    sketch += '<script type="module" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/addons/p5.sound.min.js"></script>'
     sketch += '<script type="module" src="https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm"></script>'
     # sketch += '<script type="module"> import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm";</script>'
     
