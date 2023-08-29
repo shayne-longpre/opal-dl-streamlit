659a05281ccaecd18b5a320411e2f803fca4dcdb
Shayne Longpre <srlongpre13@gmail.com>
2023-08-28 21:13:57 -0400

    update

--- run_streamlit.py
+++ run_streamlit.py
@@ -16,6 +16,7 @@ import util
 import filter_utils
 from src.helpers import io
 from src import constants
+from html_utils import compose_html_component, setup_table
 
 import streamlit as st
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
@@ -54,90 +55,8 @@ def insert_main_viz():
     sketch += '</script>'
     components.html(sketch, height=800, scrolling=True)
 
-# def insert_plot_viz1():
-#     sketch = '<div id="container"></div>'
-#     sketch += '<script type="module" src="https://d3js.org/d3.v5.min.js"></script>'
-#     sketch += '<script type="module" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/p5.js"></script>'
-#     sketch += '<script type="module" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/addons/p5.sound.min.js"></script>'
-#     sketch += '<script type="module" src="https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm"></script>'
-#     # sketch += '<link rel="stylesheet" type="text/css" href="style.css">'
-#     sketch += '<style>' + open("static2/style.css", 'r', encoding='utf-8').read() + '</style>'
-#     # sketch += '<script type="module"> import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm";</script>'
-    
-#     sketch += '<script>'
-#     sketch += "const JSONDATA = " + open("static2/data_summary.json", 'r', encoding='utf-8').read() + "\n"
-#     sketch += open("static2/plot.js", 'r', encoding='utf-8').read()
-#     sketch += '</script>'
-#     components.html(sketch, height=800, scrolling=True)
-
-def insert_plot_viz2():
-    sketch = '<div id="container"></div>'
-    sketch += '<script type="module" src="https://d3js.org/d3.v5.min.js"></script>'
-    sketch += '<script type="module" src="https://cdn.jsdelivr.net/npm/d3@7/+esm"></script>'
-    sketch += '<script type="module" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/p5.js"></script>'
-    sketch += '<script type="module" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/addons/p5.sound.min.js"></script>'
-    sketch += '<script type="module" src="https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm"></script>'
-
-    sketch += '<script src="https://unpkg.com/topojson@3"></script>'
-    sketch += '<script src="https://unpkg.com/topojson-client@3"></script>'
-
-    sketch += '<style>' + open("static2/style.css", 'r', encoding='utf-8').read() + '</style>'
-    # sketch += '<script type="module"> import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm";</script>'
-    
-    sketch += '<script>'
-    sketch += "const JSONDATA = " + open("static2/data_summary.json", 'r', encoding='utf-8').read() + "\n"
-
-    sketch += "const world = " + open("static2/countries-50m.json", 'r', encoding='utf-8').read() + "\n"
-    sketch += "const countryCodes = " + open("static2/country-codes.json", 'r', encoding='utf-8').read() + "\n"
-    sketch += "const langCodes = " + open("static2/language-codes.json", 'r', encoding='utf-8').read() + "\n"
-    sketch += "const countryCodeToLangCodes = " + open("static2/country-code-to-language-codes.json", 'r', encoding='utf-8').read() + "\n"
-
-    sketch += open("static2/worldmap.js", 'r', encoding='utf-8').read()
-    sketch += '</script>'
-    sketch += '<script>'
-    sketch += "const JSONDATA = " + open("static2/data_summary.json", 'r', encoding='utf-8').read() + "\n"
-    sketch += open("static2/plot.js", 'r', encoding='utf-8').read()
-    sketch += '</script>'
-    components.html(sketch, height=800, scrolling=True)
 
 
-def setup_table(selected_data):
-
-    # selected_data = data[_PRESENT_COLS]
-
-    custom_css = {".ag-header-cell-text": {"font-size": "12px", 'text-overflow': 'revert;', 'font-weight': 700},
-                ".ag-theme-streamlit": {'transform': "scale(0.8)", "transform-origin": '0 0'}}
-    gb = GridOptionsBuilder.from_dataframe(selected_data, axis=1)
-    gb.configure_default_column(cellStyle={'color': 'black', 'font-size': '12px'}, suppressMenu=True, wrapHeaderText=True, autoHeaderHeight=True)
-    gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
-    gb.configure_side_bar() #Add a sidebar
-    # gb.configure_selection('single', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
-    gridOptions = gb.build()
-
-    # https://streamlit-aggrid.readthedocs.io/en/docs/AgGrid.html
-    # Highlight Rows: https://discuss.streamlit.io/t/aggrid-is-there-a-way-to-color-a-row-based-on-a-column-value/20750/2
-    grid_response = AgGrid(
-        selected_data,
-        gridOptions=gridOptions,
-        custom_css=custom_css, allow_unsafe_jscode=True,
-        data_return_mode='AS_INPUT', 
-        # update_mode='MODEL_CHANGED', 
-        update_mode='NO_UPDATE', 
-        # update_mode='SELECTION_CHANGED',
-        fit_columns_on_grid_load=False,
-        theme='material', # streamlit, material
-        height=2000, 
-        width='100%',
-        # reload_data=True
-    )
-
-    # resp_data = grid_response['data']
-    # selected_rows = grid_response['selected_rows']
-
-    # if selected_rows:
-
-    #     row_index = selected_rows[0]["rowIndex"]
-    #     selected_info = resp_data.iloc[row_index]
 
 def display_metrics(metrics, df_metadata):
     metric_columns = st.columns(4)
@@ -162,7 +81,14 @@ def streamlit_app():
 
     df_metadata = util.compute_metrics(INFO["data"])
 
-    insert_plot_viz2()
+    compose_html_component("plot.js", {})
+    compose_html_component(
+        "worldmap.js", {
+            "world": "countries-50m.json",
+            "countryCodes": "country-codes.json",
+            "langCodes": "language-codes.json",
+            "countryCodeToLangCodes": "country-code-to-language-codes.json",
+        })
 
     with st.sidebar:
         st.markdown("""Select the preferred criteria for your datasets.""")
