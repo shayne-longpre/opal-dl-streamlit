ca9ada39942e989744211949fb571351435efad3
Shayne Longpre <shayne@Shaynes-MacBook-Pro.local>
2023-10-15 23:23:48 -0400

    wip

--- run_streamlit.py
+++ run_streamlit.py
@@ -34,7 +34,7 @@ def load_constants():
 def load_data():
     data_summary = io.read_data_summary_json("data_summaries/")
     data_summary = filter_util.map_license_criteria(data_summary, INFO["constants"])
-    st.write(data_summary.columns)
+    st.write(data_summary[0].keys())
     return pd.DataFrame(data_summary).fillna("")
 
 
