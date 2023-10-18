860f8877abdde2dbabfca0acb16c3030d63f1fba
Shayne Longpre <shayne@Shaynes-MacBook-Pro.local>
2023-10-17 20:58:32 -0400

    wip

--- run_streamlit.py
+++ run_streamlit.py
@@ -399,9 +399,9 @@ def streamlit_app():
     with tab4:
 
         st.header("Data Licenses :vertical_traffic_light:")
-        tab4_intro = """This section explores the *self-reported* data licenses by the creators of each dataset.
+        st.write("This section explores the *self-reported* data licenses by the creators of each dataset.")
 
-        Note a few important limitations:
+        tab4_intro = """Note a few important limitations:
         
         * The legal status of data licenses is not always clear and may be different by jurisdiction.
         * Despite our best efforts, omissions or mistakes are possible.
